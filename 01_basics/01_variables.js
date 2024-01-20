01cd2f2575fab8f15c8d826a3b7e89c0dfa47995
Djay2111 <157199074+Djay2111@users.noreply.github.com>
2024-01-20 07:31:54 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const customerId=05664;
+let custEmail="test.com";
+var custPassword="6595";
+let custState;
+
+
+custEmail="djay@1234"
+console.log(customerId)
+console.table([customerId,custEmail,custPassword,custState])
